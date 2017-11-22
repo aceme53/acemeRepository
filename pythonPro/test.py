1ac51eb55f2bb2b53b9f67c833c3c5a7ea896f77
aceme53 <284420440@qq.com>
2017-11-22 17:51:36 +0800

    python代码

--- pythonPro/test.py
+++ pythonPro/test.py
@@ -15,15 +15,23 @@ cv2.imshow("newWin", newImg)
 # cv2.imwrite('test_imwrite.jpg', color_img, (cv2.IMWRITE_JPEG_QUALITY, 80))
 # cv2.IMWRITE_PNG_COMPRESSION指定png质量，范围0到9，默认3，越高文件越小，画质越差
 # cv2.imwrite('test_imwrite.png', color_img, (cv2.IMWRITE_PNG_COMPRESSION, 5))
-
-if cv2.waitKey(0) == 27:
+key = cv2.waitKey(0)
+if key == 27:
     print('esc')
     cv2.destroyAllWindows()
-elif cv2.waitKey(0) == ord('s'):
+elif key == ord('s'):
     print('save')
     cv2.imwrite('./newImg/saveNew.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
     cv2.destroyAllWindows()
 else:
-    print('else')
+    print('else: ' + str(key))
 
+b, g, r = cv2.split(newImg)
+merged = cv2.merge((b, g, r))
 
+cv2.imshow("Blue", b)
+cv2.imshow("Green", g)
+cv2.imshow("Red", r)
+cv2.imshow("Merged", merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
