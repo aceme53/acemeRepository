afb464a8fe13d3159e26b4f01fe3dd5e41f4ec03
aceme53 <284420440@qq.com>
2017-11-23 15:55:36 +0800

    python代码

--- pythonPro/testImg.py
+++ pythonPro/testImg.py
@@ -1,20 +1,25 @@
-# -*- coding: utf-8 -*-
+# coding=utf-8
 import cv2
 import numpy as np
 
 img = cv2.imread("aceme.jpg")
 imgGrey = cv2.imread("aceme.jpg", 0)
-emptyImage = np.zeros(img.shape, np.uint8)
-print(img.shape, imgGrey.shape, emptyImage.shape)
 newImg = cv2.resize(img, (200, 200))
-newImg = cv2.copyMakeBorder(newImg, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(201, 124, 1))
+newImg = cv2.copyMakeBorder(newImg, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=(201, 124, 1))
 
-cv2.imshow("newWin", newImg)
+emptyImage = np.ones(img.shape, np.uint8)
+emptyImage = cv2.resize(emptyImage, (200, 200))
 
+cv2.imshow("newImg", newImg)
+cv2.imshow("emptyImage", emptyImage)
+
+'''
 # cv2.IMWRITE_JPEG_QUALITY指定jpg质量，范围0到100，默认95，越高画质越好，文件越大
 # cv2.imwrite('test_imwrite.jpg', color_img, (cv2.IMWRITE_JPEG_QUALITY, 80))
 # cv2.IMWRITE_PNG_COMPRESSION指定png质量，范围0到9，默认3，越高文件越小，画质越差
 # cv2.imwrite('test_imwrite.png', color_img, (cv2.IMWRITE_PNG_COMPRESSION, 5))
+'''
+
 key = cv2.waitKey(0)
 if key == 27:
     print('esc')
