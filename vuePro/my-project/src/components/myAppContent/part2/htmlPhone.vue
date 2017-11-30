7ccfcb37b4f81a7a9b08e635205b63fdd72570ad
aceme53 <284420440@qq.com>
2017-11-30 09:56:14 +0800

    htmlphone

--- vuePro/my-project/src/components/myAppContent/part2/htmlPhone.vue
+++ vuePro/my-project/src/components/myAppContent/part2/htmlPhone.vue
@@ -7,8 +7,9 @@
   .v_middle { display: flex; justify-content: center; align-items: center; }
   /*=============================================================*/
   .phone { height: 100%; }
-  .phone_border { height: 80%; width: 350px; border: 5px #000000 solid; margin: 20px auto; border-radius: 15px; box-shadow: 0 0 25px #0F0F0F }
-  .phone_headerBar { float: left; height: 5%; border-top-left-radius: 10px; border-top-right-radius: 8px;
+  .phone_border { height: 80%; width: 350px; margin: 20px auto; border-radius: 15px; box-shadow: 0 0 5px #000000, 0 0 25px #0F0F0F;
+    border: 5px #353535 solid; background-color: #353535; }
+  .phone_headerBar { float: left; height: 5%; border-top-left-radius: 10px; border-top-right-radius: 10px;
     background-image: linear-gradient(to right bottom, #AA8984 30%, #8E716D 100%); }
   .front_lamp { width: 1.2%; height: 15%; left: 20%; top: 40%;
     background-color: transparent; }
@@ -27,10 +28,11 @@
     background-image: url("../../../images/phoneImgs/frontCam.png") }
   .front_lightSensor { width: 2.2%; height: 30%; left: 80%; top: 36%; background-repeat: no-repeat; background-size: cover;
     background-image: url("../../../images/phoneImgs/lightSensor.png") }
-  .phone_screen { width: 100%; height: 85%; float: left; }
+  .phone_screenBordder { width: 100%; height: 85%; float: left; padding: 5px; }
+  .phone_screen { width: 100%; height: 100%; }
   .startup_screenPic { background-color: #CCCCCC; }
   .shutdown_screenPic { background-image: linear-gradient(to right bottom, #282828 20%, #191919 40%, #0A0A0A 60%, #000000 100%); }
-  .phone_bottomBar { float: left; height: 10%; border-bottom-left-radius: 10px; border-bottom-right-radius: 8px;
+  .phone_bottomBar { float: left; height: 10%; border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;
     background-image: linear-gradient(to right bottom, #AA8984 30%, #8E716D 60%, #7B5F5C 80%, #583E3D 100%); }
   .logo_text { color: #CACACA; font-weight: bold; text-align: center; height: 100%; }
   .phone_headerBar, .phone_bottomBar { width: 100%; background-color: #AA8984; }
@@ -39,7 +41,7 @@
   <div class="phone">
     <div class="phone_border">
       <div class="phone_headerBar p_r">
-        <div class="front_lamp p_a circle " :class="{
+        <div class="front_lamp p_a circle " v-bind:class="{
           'green_lamp': front_lamp.gLamp,
           'red_lamp': front_lamp.rLamp,
           'blue_lamp':front_lamp.bLamp,
@@ -48,9 +50,11 @@
         <div class="front_cam circle p_a bg_t"></div>
         <div class="front_lightSensor circle p_a bg_t"></div>
       </div>
-      <div class="phone_screen bg_b"
-           :class="{'startup_screenPic':screenPic.startupPic,'shutdown_screenPic':screenPic.shutdownPic}">
-        <canvas id="screenCanvas"></canvas>
+      <div class="phone_screenBordder bg_b">
+        <div class="phone_screen"
+             v-bind:class="{'startup_screenPic':screenPic.startupPic,'shutdown_screenPic':screenPic.shutdownPic}">
+          <canvas id="screenCanvas"></canvas>
+        </div>
       </div>
       <div class="phone_bottomBar">
         <div class="logo_text v_middle">HUAWEI</div>
@@ -109,20 +113,27 @@
         this.front_lamp[lamp] = 1;
       },
       getStartupAnimation: function () {
-        let cnt = Math.PI / 180;
+        let self = this;
+        screenCanvas.style.display = '';
+        screenCanvas.width = parseFloat(window.getComputedStyle(screenCanvas.parentElement).width);
+        screenCanvas.height = parseFloat(window.getComputedStyle(screenCanvas.parentElement).height);
+        let cnt = -Math.PI / 2;
         let ct = screenCanvas.getContext('2d');
-        let inter = setInterval(function () {
-          ct.beginPath();
-          ct.arc(screenCanvas.width / 2, 100, 50, 0, cnt);
-          ct.stroke();
-          ct.closePath();
-          if (cnt.toFixed(2) === (2 * Math.PI).toFixed(2)) {
-            clearInterval(inter);
-            ct.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
-            screenCanvas.style.display = 'none';
-          }
-          cnt += Math.PI / 180;
-        }, 50)
+        let drawCircle = function () {
+          let inter = setInterval(function () {
+            ct.beginPath();
+            ct.arc(screenCanvas.width / 2, screenCanvas.height / 2, screenCanvas.width / 4, -Math.PI / 2, cnt);
+            ct.stroke();
+            ct.closePath();
+            if (cnt.toFixed(2) > (3 * Math.PI / 2).toFixed(2)) {
+              clearInterval(inter);
+              ct.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
+              screenCanvas.style.display = 'none';
+            }
+            cnt += Math.PI / 180;
+          }, 10)
+        };
+        drawCircle();
       }
     },
     watch: {
